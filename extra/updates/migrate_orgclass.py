a6e78391c7ea8b6c590352989170873331e1dcb6
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2019-04-17 20:07:30 +0300

    some pep8 on migrate_orgclass

--- extra/updates/migrate_orgclass.py
+++ extra/updates/migrate_orgclass.py
@@ -2,10 +2,12 @@
 
 import subprocess
 
-search_orgs_cmd = 'ldapsearch -LLL -h {server} -s sub -D "{user_dn}" -w {password} -x -b {base_dn} dn'
-modify_cmd = 'ldapmodify -x -c -h {server} -D "{user_dn}" -w {password} -f /tmp/out.ldiff'
+search_orgs_cmd = 'ldapsearch -LLL -h {server} -s sub -D "{user_dn}" '\
+    '-w {password} -x -b {base_dn} dn'
+modify_cmd = 'ldapmodify -x -c -h {server} -D "{user_dn}" -w {password}'\
+    ' -f /tmp/out.ldiff'
 
-operation="""{dn}
+operation = """{dn}
 changetype: modify
 add: objectClass
 objectClass: eionetOrganisation
@@ -20,20 +22,23 @@ server = 'ldap.eionet.europa.eu'
 def main(server, write_password, password):
     base_dn = "ou=Organisations,o=EIONET,l=Europe"
 
-    search_cmd = search_orgs_cmd.format(server=server, user_dn=no_limits_user_dn,
-                                        password=password, base_dn=base_dn)
-    out = subprocess.Popen(search_cmd, stdout=subprocess.PIPE, shell=True).stdout.read()
+    search_cmd = search_orgs_cmd.format(
+        server=server, user_dn=no_limits_user_dn, password=password,
+        base_dn=base_dn)
+    out = subprocess.Popen(
+        search_cmd, stdout=subprocess.PIPE, shell=True).stdout.read()
     dns = [l for l in out.split('\n') if l.strip()]
 
     with open("/tmp/out.ldiff", "w") as outf:
         for dn in dns:
-            if not "cn=" in dn:
+            if "cn=" not in dn:
                 continue
             s = operation.format(dn=dn)
             outf.write(s)
 
-    cmd = modify_cmd.format(server=server, user_dn=write_access_user_dn, password=write_password)
-    subprocess.Popen(cmd, shell=True)   #, stdout=subprocess.PIPE, shell=True)
+    cmd = modify_cmd.format(server=server, user_dn=write_access_user_dn,
+                            password=write_password)
+    subprocess.Popen(cmd, shell=True)   # , stdout=subprocess.PIPE, shell=True)
 
 
 if __name__ == "__main__":
@@ -54,7 +59,9 @@ if __name__ == "__main__":
     # server = args.server
     # user_dn = args.user_dn
 
-    password = raw_input("Enter password for user '{}': ".format(no_limits_user_dn))
-    write_password = raw_input("Enter password for user '{}': ".format(write_access_user_dn))
+    password = raw_input(
+        "Enter password for user '{}': ".format(no_limits_user_dn))
+    write_password = raw_input(
+        "Enter password for user '{}': ".format(write_access_user_dn))
 
     main(server, write_password, password)
