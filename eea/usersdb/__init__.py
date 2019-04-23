9df8d8307d703bbc9636c86126de97a4b1bc4297
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2019-04-23 18:24:24 +0300

    trick pyflakes ref. defined but not used error

--- eea/usersdb/__init__.py
+++ eea/usersdb/__init__.py
@@ -1,5 +1,14 @@
 from db_agent import UsersDB
-from db_agent import editable_user_fields, editable_org_fields
-from db_agent import (OrgRenameError, NameAlreadyExists, RoleNotFound,
-                      UserNotFound)
+from db_agent import editable_user_fields
+from db_agent import editable_org_fields
+from db_agent import OrgRenameError
+from db_agent import NameAlreadyExists
+from db_agent import RoleNotFound
+from db_agent import UserNotFound
 from schema import user_info_schema
+
+# this is just to avoid pyflakes raising 'defined but not used' error
+__all__ = [UsersDB.__name__, editable_user_fields.__name__,
+           editable_org_fields.__name__, OrgRenameError.__name__,
+           NameAlreadyExists.__name__, RoleNotFound.__name__,
+           UserNotFound.__name__, user_info_schema.__name__]
