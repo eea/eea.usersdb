6b30b2471ab48d8c67ceb06f45398a3e19210a89
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-02-13 18:13:15 +0200

    Add missing fields in setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ VERSION = open(os.path.join(*PATH)).read().strip()
 setup(
     name=NAME,
     version=VERSION,
+    description="EEA Users DB",
+    long_description_content_type="text/x-rst",
+    long_description=(
+        open("README.rst").read() + "\n" +
+        open("CHANGELOG.rst").read()
+    ),
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     packages=find_packages(),
